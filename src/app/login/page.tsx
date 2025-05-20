4a162c31d640ba9e6009c9c63a9771e1c946a1f4
Feecon <mail2feeconbehera@gmail.com>
2025-05-21 04:14:10 +0530

    login page mob responsiveness fixed

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -6,10 +6,10 @@ import AuthForm from "@/components/authentication/AuthForm";
 
 const AuthenticationPage = () => {
   return (
-    <main className="h-screen grid grid-cols-2 relative">
-      <div className="relative w-full flex flex-col bg-mutated p-10 text-primary-foreground">
-        <div className="w-full h-[30%] bg-gradient-to-t from-transparent to-black/50 absolute top-0 left-0 z-10" />
-        <div className="w-full h-[40%] bg-gradient-to-b from-transparent to-black/50 absolute bottom-0 left-0 z-10" />
+    <main className="min-h-screen grid grid-cols-1 md:grid-cols-2 relative ">
+      <div className="relative w-full flex flex-col bg-mutated p-6 md:p-10 text-primary-foreground min-h-[300px]">
+        <div className="w-full h-[20%] md:h-[30%] bg-gradient-to-t from-transparent to-black/50 absolute top-0 left-0 z-10" />
+        <div className="w-full h-[25%] md:h-[40%] bg-gradient-to-b from-transparent to-black/50 absolute bottom-0 left-0 z-10" />
         <Image
           src={AuthImg}
           alt="Authentication"
@@ -21,17 +21,17 @@ const AuthenticationPage = () => {
         </div>
         <div className="relative z-20 mt-auto">
           <blockquote className="space-y-2">
-            <p className="text-lg">
+            <p className="text-base md:text-lg">
               &ldquo;Pictoria AI is a game changer for me. I have been able to
               generate high quality professional headshots within minutes. It
               has saved me countless hours of work and cost as well.&rdquo;
             </p>
-            <footer className="text-sm">David S.</footer>
+            <footer className="ttext-xs md:text-sm">David S.</footer>
           </blockquote>
         </div>
       </div>
-      <div className="relative flex flex-col items-center justify-center p-8 h-full w-full ">
-        <div className="w-[350px] mx-auto max-w-xl">
+      <div className="relative flex flex-col items-center justify-center p-4 md:p-8 h-full w-full">
+        <div className="w-full max-w-xs md:w-[350px] mx-auto md:max-w-xl">
           <AuthForm />
         </div>
       </div>
